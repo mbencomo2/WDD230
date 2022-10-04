ccf6c8dd515a08e71d57fb2b598a1d4109071257
mbencomo2 <matt.ibencomo@gmail.com>
2022-10-04 16:12:46 -0700

    Completed w04 practice

---
+++ start-navigation/js/scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('burgerBtn').addEventListener('click', () => { 
+  document.getElementById('primaryNav').classList.toggle('open');
+  document.getElementById('burgerBtn').classList.toggle('open');
+})
