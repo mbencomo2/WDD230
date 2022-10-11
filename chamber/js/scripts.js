42b2797c20e7024b70f70688c28dd551ee08a8a8
mbencomo2 <matt.ibencomo@gmail.com>
2022-10-11 00:48:35 -0700

    Started work on w05 assignment

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -13,3 +13,12 @@ const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
 datefield.innerHTML = `${fulldate}`;
 document.querySelector('#year').textContent = now.getFullYear()
+
+//Display Banner when date is Monday or Tuesday
+if (now.getDay() == 2 || now.getDay() == 3) {
+  const banner = document.createElement('p');
+  banner.id = 'banner'
+  banner.classList = 'rounded-corners'
+  banner.textContent = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.';
+  document.querySelector('header').appendChild(banner);
+};
