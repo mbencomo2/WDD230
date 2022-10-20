0d09309ee52f0f69ffb706e09911f0ae14595c59
mbencomo2 <matt.ibencomo@gmail.com>
2022-10-20 16:45:25 -0700

    moved windchill to seperate script

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -22,21 +22,3 @@ if (now.getDay() == 1 || now.getDay() == 2) {
   banner.textContent = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.';
   document.querySelector('header').appendChild(banner);
 };
-
-
-//windchill calculator
-const s = document.querySelector('#windSpeed').textContent;
-const t = document.querySelector('#temperature').textContent;
-let chill = document.querySelector('#windChill');
-if (t <= 10) {
-  chill.innerHTML = `${windChillCalc(t, s)}&deg; C`;
-}
-else {
-  chill.innerHTML = 'N/A&deg; C'
-}
-
-function windChillCalc(t, s) {
-  let windChillTemp = 35.74 + 0.6215 * t - 35.75 * s ** (0.16) + 0.4275 * t * s ** (0.16);
-  windChillTemp = Math.round(windChillTemp);
-  return windChillTemp;
-}
