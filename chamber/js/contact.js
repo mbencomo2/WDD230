917d350141d73205095b65987607a2341585da72
mbencomo2 <matt.ibencomo@gmail.com>
2022-11-26 16:08:11 -0700

    js amd html complete for chamber site

---
+++ chamber/js/contact.js
@@ -0,0 +1,9 @@
+import { pageFunctions } from "./modules.js";
+//Display a banner on Monday and Tuesday
+pageFunctions.displayBanner();
+//Handle the hamburgerMenu button
+pageFunctions.hamburgerMenu();
+//Display the date in the header
+pageFunctions.displayDate();
+//Display last document modified date
+pageFunctions.lastModified();
