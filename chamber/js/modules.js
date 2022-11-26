7dcfc3901bc235f8bcb45b0687a94b438fb71c1d
mbencomo2 <matt.ibencomo@gmail.com>
2022-11-25 17:25:38 -0700

    removded spotlight code

--- chamber/js/modules.js
+++ chamber/js/modules.js
@@ -84,21 +84,6 @@ const pageFunctions = {
       }
     }
     getWeatherData();
-  },
-  getSpotlights() {
-    const apiURL = "./data/data.json"
-
-    //Go fetch it and then wait for a response.
-    async function loadSpotlights() {
-      const response = await fetch(apiURL);
-      if (response.ok) {
-        // the API will send us JSON...but we have to convert the response before we can use it
-        // .json() also returns a promise...so we await it as well.
-        const directory = await response.json();
-        outputSpotlights(directory.members);
-      }
-    }
-    loadSpotlights();
   }
 };
 
@@ -130,46 +115,6 @@ function output(result) {
   else {
     chill.innerHTML = 'N/A';
   }
-}
-
-function outputSpotlights(data) {
-  let num = 1;
-  const goldMembers = []
-  data.forEach((member) => {
-    if (member.membership == "Gold") {
-      goldMembers.push(member)
-    }
-  });
-  goldMembers.forEach((member) => {
-    if (member.membership == "Gold" && num <= 2) {
-      const spotlight = document.createElement('div');
-      const name = document.createElement('h2');
-      const logo = document.createElement('img');
-      const spacer = document.createElement('hr');
-      const ul = document.createElement('ul');
-      const link = document.createElement('li');
-      const phone = document.createElement('li');
-    
-      name.textContent = member.name;
-      logo.setAttribute('src', 'https://via.placeholder.com/150');
-      logo.setAttribute('data-src', member.logoURL);
-      logo.setAttribute('alt', `${member.name} Logo`);
-      link.innerHTML = `<a href="${member.webAddress}">${member.webAddress}</a>`;
-      phone.innerHTML = `<a href="${member.phone}">${member.phone}</a>`;
-
-      ul.appendChild(link);
-      ul.appendChild(phone);
-
-      spotlight.appendChild(name);
-      spotlight.appendChild(logo);
-      spotlight.appendChild(spacer);
-      spotlight.appendChild(ul)
-      spotlight.id = `spotlight${num}`;
-      spotlight.classList.add('content', 'text-shadow', 'drop-shadow', 'rounded-corners');
-      document.querySelector('.container').appendChild(spotlight);
-      num = num + 1;
-    }
-  })
 }
 
 function getSprite(time, weather) {
